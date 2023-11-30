1a8c124d4a592860bbe7eaeaeae30860285c427c
Angel Aguilera <angelaguilerapa@gmail.com>
2023-11-30 01:23:02 -0300

    Cambios minimos en padding css

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -29,3 +29,16 @@ document.addEventListener("DOMContentLoaded", function() {
     speed: 50,
     waitUntilVisible: true,
 }).go();
+
+const cards = document.querySelectorAll(".card");
+const wrapper = document.querySelector(".cards");
+
+wrapper.addEventListener("mousemove", (event) => {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    card.style.setProperty("--xPos", `${x}px`);
+    card.style.setProperty("--yPos", `${y}px`);
+  });
+});
