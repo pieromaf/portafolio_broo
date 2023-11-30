f2153c633a8efcac6b9382395b8ad6e4b2c90626
Angel Aguilera <angelaguilerapa@gmail.com>
2023-11-29 21:43:42 -0300

    Mejora footer y JS con botón

---
+++ assets/js/buttonscript.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var botonIrArriba = document.getElementById("botonIrArriba");
+  
+    window.onscroll = function() {
+      mostrarBotonIrArriba();
+    };
+  
+    botonIrArriba.onclick = function() {
+      irArriba();
+    };
+  
+    function mostrarBotonIrArriba() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        botonIrArriba.style.display = "block";
+      } else {
+        botonIrArriba.style.display = "none";
+      }
+    }
+  
+    function irArriba() {
+      document.body.scrollTop = 0; // Para Safari
+      document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+    }
+  });
+  
